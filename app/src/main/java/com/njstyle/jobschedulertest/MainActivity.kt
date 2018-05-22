c71b5dfe22ab9bca8f512951bd1221ace0ee4670
njstyle <njstyle85@gmail.com>
2018-05-22 10:31:53 +0900

    Add more listener for checking connect

--- app/src/main/java/com/njstyle/jobschedulertest/MainActivity.kt
+++ app/src/main/java/com/njstyle/jobschedulertest/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.njstyle.jobschedulertest
 
-import android.app.job.JobInfo
-import android.app.job.JobScheduler
-import android.content.ComponentName
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.njstyle.jobschedulertest.job.ConnectivityJob
-import java.util.concurrent.TimeUnit
+import com.njstyle.jobschedulertest.job.ConnectivityJob.Companion.connectivityCheckJobSchedule
 
 class MainActivity : AppCompatActivity() {
-    val jobScheduler by lazy { getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler}
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val jobInfo = JobInfo.Builder(0, ComponentName(applicationContext, ConnectivityJob::class.java))
-                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                .setPersisted(true)
-                .build()
-
-        jobScheduler.schedule(jobInfo)
+        connectivityCheckJobSchedule(this)
     }
 }
--- app/src/main/java/com/njstyle/jobschedulertest/job/ConnectivityJob.kt
+++ app/src/main/java/com/njstyle/jobschedulertest/job/ConnectivityJob.kt
@@ -1,12 +1,12 @@
 package com.njstyle.jobschedulertest.job
 
+import android.app.job.JobInfo
 import android.app.job.JobParameters
+import android.app.job.JobScheduler
 import android.app.job.JobService
+import android.content.ComponentName
 import android.content.Context
-import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
+import android.net.*
 import android.net.wifi.WifiManager
 import com.njstyle.jobschedulertest.common.LogMgr
 
@@ -19,23 +19,69 @@ class ConnectivityJob: JobService() {
             override fun onCapabilitiesChanged(network: Network?, networkCapabilities: NetworkCapabilities?) {
                 super.onCapabilitiesChanged(network, networkCapabilities)
 
-                LogMgr.d()
+                print("onCapabilitiesChanged")
+            }
+
+            override fun onLinkPropertiesChanged(network: Network?, linkProperties: LinkProperties?) {
+                super.onLinkPropertiesChanged(network, linkProperties)
+
+                print("onLink")
+            }
+
+//            override fun onAvailable(network: Network?) {
+//                super.onAvailable(network)
+//
+//                print("onAvailable")
+//            }
+
+            override fun onLost(network: Network?) {
+                super.onLost(network)
+
+                print("onLost")
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+
+                print("onUnavailable")
+            }
+
+            override fun onLosing(network: Network?, maxMsToLive: Int) {
+                super.onLosing(network, maxMsToLive)
+
+                print("onLosing")
             }
         }
     }
 
-    private fun print(functionName: String) {
-        val networkInfo = connectivityManager.activeNetworkInfo
+    companion object {
+        fun connectivityCheckJobSchedule(context: Context) {
+            val jobScheduler = context.applicationContext.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
 
-        if (networkInfo.type == ConnectivityManager.TYPE_WIFI) {
-            LogMgr.d("$functionName -> ssid: ${wifiManager.connectionInfo.ssid}")
+            val jobInfo = JobInfo.Builder(0, ComponentName(context.applicationContext, ConnectivityJob::class.java))
+                    .setMinimumLatency(1000)
+                    .setOverrideDeadline(2000)
+                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                    .setPersisted(true)
+                    .build()
 
+            jobScheduler.schedule(jobInfo)
         }
+    }
 
-        LogMgr.d("$functionName -> type: ${networkInfo.type}")
-        LogMgr.d("$functionName -> isConnected: ${networkInfo.isConnected}")
-        LogMgr.d("$functionName -> subtypeName: ${networkInfo.subtypeName}")
-        LogMgr.d("$functionName -> typeName: ${networkInfo.typeName}")
+    private fun print(functionName: String) {
+        connectivityManager.activeNetworkInfo?.let{
+            if (it.type == ConnectivityManager.TYPE_WIFI) {
+                LogMgr.d("$functionName -> ssid: ${wifiManager.connectionInfo.ssid}")
+            }
+
+            LogMgr.d("$functionName -> type: ${it.type}")
+            LogMgr.d("$functionName -> isConnected: ${it.isConnected}")
+            LogMgr.d("$functionName -> subtypeName: ${it.subtypeName}")
+            LogMgr.d("$functionName -> typeName: ${it.typeName}")
+
+            LogMgr.d(" ")
+        }
     }
 
     override fun onStopJob(jobParameters: JobParameters?): Boolean {
@@ -46,9 +92,9 @@ class ConnectivityJob: JobService() {
     }
 
     override fun onStartJob(jobParameters: JobParameters?): Boolean {
-        LogMgr.d()
-        print("onStartJob")
-//        connectivityManager.registerNetworkCallback(NetworkRequest.Builder().build(), networkCallback)
+        LogMgr.d("$this $networkCallback")
+//        print("onStartJob")
+        connectivityManager.registerNetworkCallback(NetworkRequest.Builder().build(), networkCallback)
 
         return true
     }
